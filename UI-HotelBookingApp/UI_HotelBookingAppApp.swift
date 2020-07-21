e5525990fa624b0b63d98680a60b9ca3777edaa8
Valentin Malissen <keisuke.tk.fd@gmail.com>
2020-07-21 14:23:29 -0400

    Initial Commit

---
+++ UI-HotelBookingApp/UI_HotelBookingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UI_HotelBookingAppApp.swift
+//  UI-HotelBookingApp
+//
+//  Created by Valentin Malissen on 2020-07-21.
+//
+
+import SwiftUI
+
+@main
+struct UI_HotelBookingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
